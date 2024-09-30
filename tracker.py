5990bb5e7d773fd3f196965b4e345747bb9aed8c
MFourB <mfourbofficial@gmail.com>
2024-09-29 20:00:57 -0700

    tracker.py ---------- The server can receive the following commands from the client:     register (player): The client sends the     register command with their player name concatinated     directly after. Then the server performs a check     that a valid player name has been given. The     server will then check to see if the player name     is already taken. If so, the server will send     a message to the client that the player name is taken.     If not, then the server will add a dictionary to the     players dictionary. The player name is the     key with 2 values called, "status" and "t-port".     The status is currently defaulted with "free".     The number of player of players is then incremented     by 1. Then a response is sent back to the user     indicating either the failure or success of     adding their player name to the game.
        de-register (player): The client sends the
        de-register command with their player name concatinated
        directly after. Then the server performs a check
        that a valid player name has been given. Then
        it checks to see if the player exists in the
        players list. Then it checks that the player
        requesting to de-register a user has the same
        t-port associated with the player in the list.
        Then the server removes the player from the list
        and decrements the number of players.
    
        query-players: The client sends the query-players
        request. The server will JSON dump the players data,
        which contains the number of players and the
        list of players. Then will send this data
        back to the user.
    
    player.py
    ---------
    On opening of the application, the player is prompted
    to register a player name. Once given, the client
    sends the request to the server. The server will
    provide a response.
    The client will then segment the
    data chunks into four categories: response_Status_Code,
    response_Command, responseMessage, and responseData.
    The response_Status_Code will decide whether to continue
    exchanges. If there is a failure, the application will close.
    If there is a success, the application will move forward.
    The response_Command lets the client know what command
    the server responded to. The responseMessage tells
    the client, in more detail, what had taken place
    with the client-server exchange. The responseData
    may contain complex data structures.
    The client will
    then be prompted with various options such as: query-players,
    start-game, query-games, and de-register.
    The following options will perform the following
        query-players: When the players sends a query-players
        request to the server. The server will return a
        response and segmented by the client. The responseData
        will contain the number of registered players and
        the list of players registered. Then the numbered
        of registered players is displayed to the user.
        As well as the list of players.
    
        query-games: When the players sends a query-games
        request to the server. The server will return a
        response and segmented by the client. The responseData
        will contain the number of in-progress games and
        the list of games in-progress. Then the numbered
        of in-progress games is displayed to the user.
        As well as the list of in-progress games.
    
        de-register: The user sends their player name
        to the server to have them removed from the
        registered player list. A response is returned
        on the success or failure of this request.
    
    In the case that the server remains unresponsive for A
    command for a certain amount of time. The client
    socket will timeout and cancel the request.

--- tracker.py
+++ tracker.py
@@ -1,10 +1,123 @@
 from socket import *
+import json
+
 serverPort = 1000
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 
+players_Data = {
+    "Number_Of_Players": 0,
+    "Players": {}
+}
+
+games_Data = {
+    "Number_Of_Games": 0,
+    "Games": {}
+}
+
+def register(player, IPv4, t_port):
+    if not (player and IPv4 and t_port):
+        print("Invalid arguments")
+
+    print("Registering", player, IPv4, t_port)
+
+    status_Code = ""
+
+    if player in players_Data["Players"]:
+        status_Code = f"FAILURE.|register.|{player} already exists!.|"
+        print(status_Code)
+        return status_Code
+
+    players_Data["Players"][player] = {
+        "status": "free",
+        "t-port": t_port
+    }
+    players_Data["Number_Of_Players"] += 1
+
+    status_Code = f"SUCCESS.|register.|{player} are registered!.|"
+    print(status_Code)
+    return status_Code
+
+def de_register(player, IPv4, t_port):
+    if not player in players_Data["Players"]:
+        status_Code = f"FAILURE.|de-register.|{player} is not registered!.|"
+        print(status_Code)
+        return status_Code
+    elif not players_Data["Players"][player]["t-port"] == t_port:
+        status_Code = f"FAILURE.|de-register.|De-register request is not coming from {player}!.|"
+        print(status_Code)
+        return status_Code
+    elif players_Data["Players"][player]["status"] == "in-play":
+        status_Code = f"FAILURE.|de-register.|{player} is in a game!.|"
+        print(status_Code)
+        return status_Code
+    
+    del players_Data["Players"][player]
+    players_Data["Number_Of_Players"] -= 1
+
+    status_Code = f"SUCCESS.|de-register.|{player} has been de-registered!.|"
+    print(status_Code)
+    return status_Code
+
+
+def queryPlayers():
+    queriedPlayers = json.dumps(players_Data)
+    #print(queriedPlayers)
+    status_Code = "SUCCESS.|query-players.|players retrieved.|"+queriedPlayers
+    print(status_Code)
+    return status_Code
+
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    print(modifiedMessage)
+    try:
+        message, clientAddress = serverSocket.recvfrom(2048)
+    except KeyboardInterrupt:
+        serverSocket.close()
+        print("Exiting")
+
+    commands_and_parameters = message.decode().lower().split(' ')
+    command = commands_and_parameters[0]
+    parameters = commands_and_parameters [1:]
+    
+    match command:
+        case "register":
+            try:
+                player = parameters[0]
+            except IndexError:
+                print("No player name given")
+                continue
+            
+            response = register(player, *clientAddress)
+            serverSocket.sendto(response.encode(),
+                                      clientAddress)
+        
+        case "query-players":
+            print("querying players")
+            response = queryPlayers()
+
+            serverSocket.sendto(response.encode(), clientAddress)
+
+        case "start-game":
+            print("starting game")
+        case "query-games":
+            print("querying games")
+        case "end":
+            print("ending game")
+        case "de-register":
+            print("De-registering")
+            try:
+                player = parameters[0]
+            except IndexError:
+                print("No player name given")
+                continue
+            
+            response = de_register(player, *clientAddress)
+            serverSocket.sendto(response.encode(), clientAddress)
+        case _:
+            status_Code = "FAILURE.|command.|Invalid command.|"
+            print(status_Code)
+            serverSocket.sendto(status_Code.encode(), clientAddress)
+
+    
+
+    #print(command, parameters, clientAddress)
