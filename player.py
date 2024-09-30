3775d24c6f6854d2a7fc4028ef8f95ef034f180b
MFourB <mfourbofficial@gmail.com>
2024-09-29 20:04:46 -0700

    client-server skeleton
    tracker.py
    ----------
    The server can receive the following commands from
    the client:
        register (player): The client sends the
        register command with their player name concatinated
        directly after. Then the server performs a check
        that a valid player name has been given. The
        server will then check to see if the player name
        is already taken. If so, the server will send
        a message to the client that the player name is taken.
        If not, then the server will add a dictionary to the
        players dictionary. The player name is the
        key with 2 values called, "status" and "t-port".
        The status is currently defaulted with "free".
        The number of player of players is then incremented
        by 1. Then a response is sent back to the user
        indicating either the failure or success of
        adding their player name to the game.
    
        de-register (player): The client sends the
        de-register command with their player name concatinated
        directly after. Then the server performs a check
        that a valid player name has been given. Then
        it checks to see if the player exists in the
        players list. Then it checks that the player
        requesting to de-register a user has the same
        t-port associated with the player in the list.
        Then the server removes the player from the list
        and decrements the number of players.
    
        query-players: The client sends the query-players
        request. The server will JSON dump the players data,
        which contains the number of players and the
        list of players. Then will send this data
        back to the user.
    
    player.py
    ---------
    On opening of the application, the player is prompted
    to register a player name. Once given, the client
    sends the request to the server. The server will
    provide a response.
    The client will then segment the
    data chunks into four categories: response_Status_Code,
    response_Command, responseMessage, and responseData.
    The response_Status_Code will decide whether to continue
    exchanges. If there is a failure, the application will close.
    If there is a success, the application will move forward.
    The response_Command lets the client know what command
    the server responded to. The responseMessage tells
    the client, in more detail, what had taken place
    with the client-server exchange. The responseData
    may contain complex data structures.
    The client will
    then be prompted with various options such as: query-players,
    start-game, query-games, and de-register.
    The following options will perform the following
        query-players: When the players sends a query-players
        request to the server. The server will return a
        response and segmented by the client. The responseData
        will contain the number of registered players and
        the list of players registered. Then the numbered
        of registered players is displayed to the user.
        As well as the list of players.
    
        query-games: When the players sends a query-games
        request to the server. The server will return a
        response and segmented by the client. The responseData
        will contain the number of in-progress games and
        the list of games in-progress. Then the numbered
        of in-progress games is displayed to the user.
        As well as the list of in-progress games.
    
        de-register: The user sends their player name
        to the server to have them removed from the
        registered player list. A response is returned
        on the success or failure of this request.
    
    In the case that the server remains unresponsive for A
    command for a certain amount of time. The client
    socket will timeout and cancel the request.

--- player.py
+++ player.py
@@ -1,8 +1,83 @@
 from socket import *
+import json
+
 serverName = "localhost"
 serverPort = 1000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input("Input lowercase sentance:")
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-# modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+clientSocket.settimeout(1)
+
+def sendRequest(request):
+    clientSocket.sendto(request.encode(), (serverName, serverPort))
+    
+    try:
+        response, serverAddress = clientSocket.recvfrom(2048)
+    except TimeoutError:
+        print("Timeout")
+        #homeMenu()
+        return
+
+    response = list(response.decode().split(".|", 3))
+    responseFormatted = {
+        "response_Status_Code": response[0],
+        "response_Command": response[1],
+        "response_Message": response[2],
+        "response_Data": response[3],
+    }
+
+    if responseFormatted["response_Status_Code"] == "SUCCESS":
+        print("\nSUCCESS", responseFormatted["response_Message"])
+    elif responseFormatted["response_Status_Code"] == "FAILURE":
+        print("\nFAILURE", responseFormatted["response_Message"])
+        exit()
+    
+    return responseFormatted
+
+def registerMenu():
+    player_Name = input("\nRegister a username: ")
+    sendRequest("register " + player_Name)
+
+    displayHomeMenu()
+
+def displayHomeMenu():
+    print("\nWelcome to the Game of Six Card Golf")
+    print("Input the desired option that follows")
+    #print("register (Player name)")
+    print("query-players")
+    print("start-game (player), (number of players), (number of holes)")
+    print("query-games")
+    print("de-register (player)")
+
+    homeMenuInputs()
+
+def homeMenuInputs():
+    option_choice = input("\nEnter option (Example: start-game JohnDoe 3 9): ")
+
+    if option_choice.split(" ")[0].lower() == "register":
+        print("FAILURE You've already registered!")
+
+    responseFormated = sendRequest(option_choice)
+
+    if responseFormated["response_Command"] == "query-players":
+        players_Data = json.loads(responseFormated["response_Data"])
+
+        print("\nNumber of Players:", players_Data["Number_Of_Players"],"""\n--------------""")
+        for player in players_Data["Players"]:
+            print(player)
+        print()
+        homeMenuInputs()
+
+    elif responseFormated["response_Command"] == "de-register":
+        print("\nNow exiting application\n")
+
+    #print(response)
+
+def main():
+    try:
+        registerMenu()
+    except KeyboardInterrupt:
+        print("Exiting")
+
+if __name__=="__main__":
+    main()
+
 clientSocket.close()
