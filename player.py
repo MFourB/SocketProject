8e050322fb481248db7a01ffd0c85bff390303be
MFourB <mfourbofficial@gmail.com>
2024-10-20 23:51:40 -0700

    Finalized start-game mechanics

--- player.py
+++ player.py
@@ -1,10 +1,92 @@
 from socket import *
 import json
+import threading
+import random
+from time import sleep
 
 serverName = "192.168.1.2"
-serverPort = 1000
+serverPort = 46500
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1)
+
+game_Port = 0
+game_Status = "Waiting"
+game_Socket = socket(AF_INET, SOCK_DGRAM)
+game_Socket.settimeout(1)
+
+player_Name = ""
+
+threads = {}
+game_Data = {}
+player_Sockets = []
+
+deck_Of_Cards = [
+    {"AD": 1},
+    {"AC": 1},
+    {"AH": 1},
+    {"AS": 1},
+    {"2D": -2},
+    {"2C": -2},
+    {"2H": -2},
+    {"2S": -2},
+    {"3D": 3},
+    {"3C": 3},
+    {"3H": 3},
+    {"3S": 3},
+    {"4D": 4},
+    {"4C": 4},
+    {"4H": 4},
+    {"4S": 4},
+    {"5D": 5},
+    {"5C": 5},
+    {"5H": 5},
+    {"5S": 5},
+    {"6D": 6},
+    {"6C": 6},
+    {"6H": 6},
+    {"6S": 6},
+    {"7D": 7},
+    {"7C": 7},
+    {"7H": 7},
+    {"7S": 7},
+    {"8D": 8},
+    {"8C": 8},
+    {"8H": 8},
+    {"8S": 8},
+    {"9D": 9},
+    {"9C": 9},
+    {"9H": 9},
+    {"9S": 9},
+    {"10D": 10},
+    {"10C": 10},
+    {"10H": 10},
+    {"10S": 10},
+    {"JD": 10},
+    {"JC": 10},
+    {"JH": 10},
+    {"JS": 10},
+    {"QD": 10},
+    {"QC": 10},
+    {"QH": 10},
+    {"QS": 10},
+    {"KD": 0},
+    {"KC": 0},
+    {"KH": 0},
+    {"KS": 0},
+]
+
+deck_Of_Cards_Copy = deck_Of_Cards.copy()
+
+def formatRequest(response):
+    response = list(response.decode().split(".|", 3))
+    responseFormatted = {
+        "response_Status_Code": response[0],
+        "response_Command": response[1],
+        "response_Message": response[2],
+        "response_Data": response[3],
+    }
+    
+    return responseFormatted
 
 def sendRequest(request):
     clientSocket.sendto(request.encode(), (serverName, serverPort))
@@ -16,26 +98,122 @@ def sendRequest(request):
         #homeMenu()
         return False
 
-    response = list(response.decode().split(".|", 3))
-    responseFormatted = {
-        "response_Status_Code": response[0],
-        "response_Command": response[1],
-        "response_Message": response[2],
-        "response_Data": response[3],
-    }
+    responseFormatted = formatRequest(response)
 
     if responseFormatted["response_Status_Code"] == "SUCCESS":
-        print("\nSUCCESS", responseFormatted["response_Message"])
+        if not responseFormatted["response_Command"] == "start-game":
+            print("\nSUCCESS", responseFormatted["response_Message"])
     elif responseFormatted["response_Status_Code"] == "FAILURE":
         print("\nFAILURE", responseFormatted["response_Message"])
-    
+
     return responseFormatted
 
-def registerMenu():
-    player_Name = input("\nRegister a username: ")
-    sendRequest("register " + player_Name)
+def gameMenu():
+    option_choice = input("huhu")
+    print("game")
+    gameMenu()
 
-    homeMenu()
+
+def waiting_Room(game_Port):
+    print("\nWaiting for a game to start")
+    global game_Data
+    global socket
+    global deck_Of_Cards_Copy
+    global player_Name
+
+    while True:
+        try:
+            #print("")
+            message, senderAddress = game_Socket.recvfrom(2048)
+        except TimeoutError:
+            continue
+
+        responseFormated = formatRequest(message)
+        #print(responseFormated)
+
+        if not responseFormated:
+            continue
+
+        match responseFormated["response_Command"]:
+            case "game_init":
+                global game_Status
+                game_Status = "In_Game"
+                print("\nNow Starting Game")
+                print("Press Enter to Once or Twice to Begin?")
+                input()
+            case "show_role":
+                print(responseFormated["response_Message"])
+            case "give_players":
+                game_Data = json.loads(responseFormated["response_Data"])
+                random.shuffle(deck_Of_Cards_Copy)
+                #print(game_Data["Players"])
+                for player in game_Data["Players"]:
+                    player_Socket = socket(AF_INET, SOCK_DGRAM)
+                    player_Sockets.append(player_Socket)
+                    player_Socket.settimeout(1) 
+                    player["cards"] = []
+                    for index in range(0,6):
+                        player["cards"].append(deck_Of_Cards_Copy.pop())
+
+                for player in game_Data["Players"]:
+                    player_Socket.sendto(("SUCCESS.|deal.|This is from the dealer.|"+json.dumps(game_Data)).encode(), (player["ipv4"], int(player["p_port"])))
+
+            case "deal":
+                game_Data = json.loads(responseFormated["response_Data"])
+                for player in game_Data["Players"]:
+                    deck_String = ""
+                    index = 0
+                    #cards = list(player["cards"]).
+                    for card in player["cards"]:
+                        if index > 1:
+                            #deck_String += cards[index] + " "
+                            break
+                        card_Value = list(card.keys())[0]
+                        deck_String += card_Value
+                        index += 1
+                    deck_String += " ***\n*** *** ***"
+                    print(deck_String)
+
+                    print()
+                print(responseFormated["response_Message"])
+                print()
+        
+        
+
+
+def registerMenu():
+    print("\nReplace the following values in parenthesis")
+    print("Player Port is unique to yourself. Range is from 46501 to 46999")
+    print("register (Player Name) (Player Port)")
+    register = input("\nregister ")
+    responseFormated = sendRequest("register " + register)
+
+    if not responseFormated\
+    or responseFormated["response_Status_Code"] == "FAILURE":
+        registerMenu()
+        return
+
+    register_Values = register.lower().split(' ')
+    player_Name = register_Values[0]
+    p_port = register_Values[1]
+
+    print(player_Name, p_port)
+    game_Port = int(p_port)
+    game_Socket.bind((gethostbyname(gethostname()), game_Port))
+    #print(p_port, gethostbyname(gethostname()), game_Socket)
+
+    threads["game_Thread"] = threading.Thread(target=waiting_Room, args=(game_Port,))
+    game_Thread = threads["game_Thread"]
+    game_Thread.daemon = True
+    game_Thread.start()
+
+    threads["home_Thread"] = threading.Thread(target=homeMenu)
+    home_Thread = threads["home_Thread"]
+    home_Thread.daemon = True
+    home_Thread.start()
+
+    game_Thread.join()
+    home_Thread.join()
 
 def homeMenu():
     print("\nWelcome to the Game of Six Card Golf")
@@ -49,8 +227,12 @@ def homeMenu():
     homeMenuInputs()
 
 def homeMenuInputs():
+    if game_Status == "In_Game":
+        return
     option_choice = input("\nEnter option (Example: start-game JohnDoe 3 9): ")
-
+    if game_Status == "In_Game":
+        return
+    
     if option_choice.split(" ")[0].lower() == "register":
         print("FAILURE You've already registered!")
 
@@ -59,6 +241,7 @@ def homeMenuInputs():
     if not responseFormated\
     or responseFormated["response_Status_Code"] == "FAILURE":
         homeMenuInputs()
+        return
 
     if responseFormated["response_Command"] == "query-players":
         players_Data = json.loads(responseFormated["response_Data"])
@@ -72,6 +255,7 @@ def homeMenuInputs():
                 print("\tIn game")
         print()
         homeMenuInputs()
+        return
     
     elif responseFormated["response_Command"] == "query-games":
         games_Data = json.loads(responseFormated["response_Data"])
@@ -86,9 +270,15 @@ def homeMenuInputs():
                 print(f"        {player['name']}, Role: {player['role']}")
         print()
         homeMenuInputs()
+        return
+
+    elif responseFormated["response_Command"] == "start-game":
+        homeMenuInputs()
+        return
 
     elif responseFormated["response_Command"] == "de-register":
         print("\nNow exiting application\n")
+        return
 
     #print(response)
 
--- tracker.py
+++ tracker.py
@@ -4,11 +4,16 @@ import threading
 import os
 import uuid
 
-serverPort = 1000
+server_Port = 46500
+
+starting_p_port_Range = 46501
+ending_p_port_Range = 46999
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('192.168.1.2', serverPort))
+serverSocket.bind(('192.168.1.2', server_Port))
 serverSocket.settimeout(1)
+
+Available_Ports = {}
 
 class Players_Queue:
     Players_Queue = []
@@ -66,7 +71,7 @@ class Players_Data:
     def toJSON(self):
         return json.dumps({
             "Number_Of_Players": self.Number_Of_Players,
-            "Players": {player: self.Players[player].__dict__ for player in self.Players}
+            "Players": {player: self.Players[player].toDict() for player in self.Players}
         })
 
 class Games_Data:
@@ -94,9 +99,24 @@ class Player(object):
     def __init__(self):
         self.name = ""
         self.status = "free"
+        self.ipv4 = ""
+        self.t_port = ""
         self.p_port = ""
         self.queue_Placement = 0
         self.role = ""
+        self.cards = []
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.settimeout(1)
+    def toDict(self):
+        return {
+            "name": self.name,
+            "status": self.status,
+            "ipv4": self.ipv4,
+            "p_port": self.p_port,
+            "queue_Placement": self.queue_Placement,
+            "role": self.role,
+            "cards": self.cards
+        }
 
 class Game(object):
     def __init__(self):
@@ -107,9 +127,11 @@ class Game(object):
     def toDict(self):
         return {
             "id": self.id,
-            "Players": [player.__dict__ for player in self.Players],
+            "Players": [player.toDict() for player in self.Players],
             "status": self.status,
         }
+    def toJSON(self):
+        return json.dumps(self.toDict())
     def add_Player(self, player, status, role):
         player.status = status
         player.role = role
@@ -119,13 +141,13 @@ class Game(object):
 def send_Server_Response(message):
     serverSocket.sendto(message.encode(), clientAddress)   
 
-def verify_Player(player, p_port, command):
+def verify_Player(player, t_port, command):
     if not player in Players_Data.Players:
         status_Code = f"FAILURE.|{command}.|{player} is not registered!.|"
         send_Server_Response(status_Code) 
         print(status_Code)
         return False
-    elif not Players_Data.Players[player].p_port == p_port:
+    elif not Players_Data.Players[player].t_port == t_port:
         status_Code = f"FAILURE.|{command}.|{command} request is not coming from {player}!.|"
         send_Server_Response(status_Code) 
         print(status_Code)
@@ -138,11 +160,23 @@ def verify_Player(player, p_port, command):
     
     return True
 
-def register(player="", IPv4="", p_port=""):
-    if not (player and IPv4 and p_port):
+def register(player="", IPv4="", t_port="", p_port=""):
+    if not (player or IPv4 or t_port or p_port):
         print("Invalid arguments")
 
-    print("Registering", player, IPv4, p_port)
+    print("Registering", player, IPv4, t_port, p_port)
+
+    try:   
+        if Available_Ports[p_port]:
+            status_Code = f"FAILURE.|register.|port {p_port} already taken!.|"
+            send_Server_Response(status_Code) 
+            print(status_Code)
+            return False
+    except KeyError:
+            status_Code = f"FAILURE.|register.|port {p_port} is not within range!.|"
+            send_Server_Response(status_Code) 
+            print(status_Code)
+            return False
 
     status_Code = ""
 
@@ -160,6 +194,8 @@ def register(player="", IPv4="", p_port=""):
     newPlayer = Player()
     newPlayer.name = player
     newPlayer.status = "free"
+    newPlayer.ipv4 = IPv4
+    newPlayer.t_port = t_port
     newPlayer.p_port = p_port
     newPlayer.queue_Placement = Players_Queue.size()
     Players_Queue.add_Player(newPlayer)
@@ -167,18 +203,21 @@ def register(player="", IPv4="", p_port=""):
     Players_Data.Number_Of_Players += 1
     Players_Data.Players[newPlayer.name] = newPlayer
 
+    Available_Ports[p_port] = True
+
     status_Code = f"SUCCESS.|register.|{player} are registered!.|"
 
     send_Server_Response(status_Code) 
 
     print(status_Code)
 
-def de_register(player, IPv4, p_port):
-    if not verify_Player(player, p_port, "de-register"):
+def de_register(player, IPv4, t_port):
+    if not verify_Player(player, t_port, "de-register"):
         return False
     
     player = Players_Data.Players[player]
     Players_Queue.consume_Player(player)
+    Available_Ports[player.p_port] = False
     del Players_Data.Players[player.name]
     Players_Data.Number_Of_Players -= 1
 
@@ -226,30 +265,42 @@ def start_Game(player, num_Of_Additional_Players, num_Of_Holes, IPv4, p_port):
     Games_Data.Games[newGame.id] = newGame
     Games_Data.Number_Of_Games += 1
 
-    player = Players_Data.Players[player]
+    dealer = Players_Data.Players[player]
 
-    newGame.add_Player(player, "in-play", "Dealer")
-    Players_Queue.consume_Player(player)
-    
+    newGame.add_Player(dealer, "in-play", "Dealer")
+    Players_Queue.consume_Player(dealer)
     for index in range(0, num_Of_Additional_Players):
         newPlayer = Players_Queue.consume_Next_Player()
+        newPlayer.socket.sendto("SUCCESS.|game_init.|Game is Starting.|".encode(), (newPlayer.ipv4, int(newPlayer.p_port)))   
+        newPlayer.socket.sendto("SUCCESS.|show_role.|You are now a player.|".encode(), (newPlayer.ipv4, int(newPlayer.p_port)))   
         newGame.add_Player(newPlayer, "in-play", "Player")
-        #Players_Queue.consume_Next_Player()
         print("Adding Player", newPlayer)
 
-    #for index in range(0,)
+    dealer.socket.sendto("SUCCESS.|game_init.|Game is Starting.|".encode(), (dealer.ipv4, int(dealer.p_port)))  
+    dealer.socket.sendto("SUCCESS.|show_role.|You are now the Dealer.|".encode(), (dealer.ipv4, int(dealer.p_port)))  
+    dealer.socket.sendto(("SUCCESS.|give_players.|Here is the player data.|"+newGame.toJSON()).encode(), (dealer.ipv4, int(dealer.p_port)))  
 
     status_Code = "SUCCESS.|start-game.|games has started.|"
-
     send_Server_Response(status_Code) 
 
-    print(status_Code, player, Players_Queue.size(), num_Of_Additional_Players)
+
+
+    #print(status_Code, player, Players_Queue.size(), num_Of_Additional_Players)
+
+def main():
+    for index in range(46501, 46999):
+        Available_Ports[str(index)] = False
+    
+    print(Available_Ports)
+
+if __name__=="__main__":
+    main()
 
 try:
     while True:
         try:
             message, clientAddress = serverSocket.recvfrom(2048)
-        except TimeoutError:
+        except TimeoutError or ConnectionResetError:
             continue
 
         commands_and_parameters = message.decode().lower().split(' ')
@@ -260,11 +311,12 @@ try:
             case "register":
                 try:
                     player = parameters[0]
+                    p_port = parameters[1]
                 except IndexError:
-                    print("No player name given")
+                    print("Invalid Register Parameters")
                     continue
                 
-                register(player, *clientAddress)
+                register(player, *clientAddress, p_port)
             
             case "query-players":
                 print("querying players")
